a7d391ab47d9103f8ed7becf07d22a22a5127a58
Gabriel Macedo Araujo <79328112+M4G1Ck@users.noreply.github.com>
2021-06-15 12:00:47 -0300

    FuncaoAnonima3

---
+++ ReactWeb/FuncaoAnonima3.js
@@ -0,0 +1,17 @@
+let add = function (a, b) {
+    return a + b;
+    }
+    
+let a = 3
+let b = 5
+let add2 = () => a + b;
+
+function adicao(a, b) {
+    return a + b;
+    }
+
+console.log( add(a, b) );
+
+console.log( add2() );
+
+console.log( adicao(a, b) );
