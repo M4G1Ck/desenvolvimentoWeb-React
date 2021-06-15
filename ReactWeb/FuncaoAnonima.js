39045983817d6af54c8271ab5dd1fee15e5dea0d
Gabriel Macedo Araujo <79328112+M4G1Ck@users.noreply.github.com>
2021-06-15 11:23:44 -0300

    FuncaoAnonima

---
+++ ReactWeb/FuncaoAnonima.js
@@ -0,0 +1,12 @@
+(function(){
+    console.log(`Chamada imediatamente`);
+})();
+
+// let pessoa = {
+// primeiramente: "Gabriel"
+// segundamente: "Gabriel tambem"
+// };
+
+// (function(){
+// console.log(`$(pessoa.primeiramente) $(pessoa.segundamente)`);
+// })(pessoa);
